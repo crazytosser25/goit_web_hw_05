2e69069c434fd79fa57e2eff9e8e6b948c288e63
YevheniiBaranov <j52b10i78@gmail.com>
2024-07-06 11:12:38 +0200

    range request add

--- app.py
+++ app.py
@@ -3,10 +3,11 @@
 Returns:
     json: Dictionaty with currencies to Grivna (UAH)
 """
-import platform
+import sys
 import asyncio
 import logging
 import json
+import platform
 from datetime import date, timedelta
 from aiohttp import ClientSession
 
@@ -17,24 +18,41 @@ logging.basicConfig(
     datefmt='[%d/%b/%Y %H:%M:%S]'
 )
 
-def format_data(row_data, filtered_currencies):
-    formatted_data = [
-        {row_data['date']: {
-            rate['currency']: {
-                'sale': rate.get('saleRate', rate.get('saleRateNB')),
-                'purchase': rate.get('purchaseRate', rate.get('purchaseRateNB'))
+def processing_arguments() -> int:
+    """This function processes command line arguments.
+
+    Returns:
+        int: Quantity of days
+    """
+    try:
+        if len(sys.argv) > 2:
+            raise IndexError("Too many arguments.")
+        days = int(sys.argv[1]) if len(sys.argv) == 2 else 1
+
+    except ValueError:
+        print("The argument must be an integer.")
+        sys.exit()
+    except IndexError as e:
+        print(e)
+        sys.exit()
+
+    return days
+
+def format_data(row_data, filtered_currencies) -> dict:
+    formatted_data = {}
+    for i in row_data['exchangeRate']:
+        if i['currency'] in filtered_currencies:
+            formatted_data[i['currency']] = {
+                'sale': i.get('saleRate'),
+                'purchase': i.get('purchaseRate')
             }
-            for rate in row_data['exchangeRate']
-            if rate['currency'] in filtered_currencies
-        }}
-    ]
-    return json.dumps(formatted_data, indent=2)
+    return {row_data['date']: formatted_data}
 
 
-async def request(today):
+async def request(day) -> list:
     async with ClientSession() as session:
         async with session.get(
-            f'https://api.privatbank.ua/p24api/exchange_rates?json&date={today}'
+            f'https://api.privatbank.ua/p24api/exchange_rates?json&date={day}'
         ) as response:
             logging.debug("Status: %s", response.status)
             logging.debug("Content-type: %s", response.headers['content-type'])
@@ -43,17 +61,25 @@ async def request(today):
             result = await response.json()
             return result
 
-async def main():
-    FILTERED_CURRENCIES = ['USD', 'EUR']
+async def main() -> list:
+    filtered_currencies = ['USD', 'EUR']
     today = date.today()
-    nedded_day = (today - timedelta(days=1)).strftime("%d.%m.%Y")
-    result = await request(nedded_day)
-    logging.debug(result)
-    return format_data(result, FILTERED_CURRENCIES)
+    days = processing_arguments()
+
+    resulted_list = []
+    for i in range(days):
+        needed_day = (today - timedelta(i)).strftime("%d.%m.%Y")
+        result = await request(needed_day)
+        logging.debug(result)
+        formatted_result = format_data(result, filtered_currencies)
+        resulted_list.append(formatted_result)
+
+    return resulted_list
 
 
 if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    print(r)
+    currencies = asyncio.run(main())
+    print("Results of request to Privatbank:")
+    print(json.dumps(currencies, indent=2))
